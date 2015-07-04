3137e24e33377ba8beb65a4ff1ef17d338df695a
eladbirnboim <eladbirn@gmail.com>
2015-07-04 11:21:38 +0300

    add graph constraints

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,3 +7,15 @@ password = 'DavidElad1'
 authenticate(url.strip('http://'), username, password)
 
 graph = Graph(url + '/db/data/')
+
+#graph constraints
+nodes = [
+    ('Item', 'tmdb_id'),
+    ('User', 'username')
+]
+
+for label, property in nodes:
+    try:
+        graph.schema.create_uniqueness_constraint(label, property)
+    except:
+        continue
