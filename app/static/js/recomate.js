52d37788c09d194c94a762b67f86e2f7ddfaddc2
eladbirnboim <eladbirn@gmail.com>
2015-07-21 00:47:38 +0300

    add star, not complete

--- app/static/js/recomate.js
+++ app/static/js/recomate.js
@@ -113,6 +113,7 @@ myApp.controller('signup', function($scope,$resource, $location){
 
 
 myApp.controller('user', function($scope, $resource, $location, $routeParams, Upload, ngDialog){
+
 	var User;
 	if($routeParams.user_id){
 		User = $resource('/user/:user_id', {user_id: $routeParams.user_id});
@@ -152,6 +153,24 @@ myApp.controller('user', function($scope, $resource, $location, $routeParams, Up
 	function getRanks(username){
 		var Ranks = $resource('/rank/:username', {username: username});
 		$scope.ranksArr =  Ranks.query();
+	}
+
+	$scope.invokeRanking = function() {
+		console.log("invokeRanking");
+		$(".input-rank").rating({
+			min: 0,
+			max: 5,
+			step: 0.01,
+			size: 'xs',
+			readonly: true,
+			showClear: false,
+			showCaption: false,
+			starCaptions: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',}
+		});
+		angular.forEach($scope.ranksArr, function(rank){
+			$('#rank'+rank.item_id).rating('update', rank.rank);
+			console.log(rank)
+		});
 	}
 
 	$scope.getReview = function(index, item_id){
@@ -242,23 +261,29 @@ myApp.controller('changeItem', function($scope,$resource, $location, $routeParam
 });
 
 myApp.controller('item', function($scope,$resource, $location, $routeParams){
+	$(".input-rank").rating({min:0, max:5, step:0.01, size:'xs', readonly:true, showClear:false, showCaption:false, starCaptions:{0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',}});
+	$("#input-user-rank").rating({min:0, max:5, step:1, size:'xs', showClear:false, showCaption:false, starCaptions:{0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',}});
 	var Item = $resource('/item/:item_id', {item_id: $routeParams.item_id});
 	Item.get(function(res){
 		$scope.item = res;
 		console.log($scope.item);
 		$scope.reviewButton = "write review";
 
+
 		if (!$scope.item.itemDetails.hasOwnProperty('poster_path')) {
 			$scope.item.itemDetails.poster_path = false;
 		}
 
 		if (res.itemRanks.rank) {
+			$('#input-user-rank').rating('update', res.itemRanks.rank);
 			$scope.myRank = res.itemRanks.rank;
 			$scope.reviewText = res.itemRanks.review_text;
 		} else {
 			$scope.myRank = 0;
 		}
 
+		$("#followingAvgRank").rating('update', $scope.item.itemRanks.followingAvg);
+		$("#totalRank").rating('update', $scope.item.itemRanks.totalAvg);
 		if($scope.item.itemRanks.followingAvg == null){
 			$scope.item.itemRanks.followingAvg = '-';
 		}
@@ -272,12 +297,19 @@ myApp.controller('item', function($scope,$resource, $location, $routeParams){
 		$location.path('/login').replace();
 	});
 
+
 	$scope.showDescription = function() {
 		if ($scope.description) {
 			$scope.description = false;
 		} else {
 			$scope.description = true;
 		}
+	}
+
+	$scope.addRankTest = function(){
+		console.log("here");
+		console.log($scope.myRank2);
+
 	}
 
 	$scope.addRank = function(){
@@ -293,6 +325,8 @@ myApp.controller('item', function($scope,$resource, $location, $routeParams){
 			});
 		}
 	}
+
+
 
 	$scope.addReview = function(){
 		$(".reviewBtn").css("background-color","white");
@@ -400,6 +434,23 @@ myApp.controller('top', function($scope,$resource, $location, $routeParams){
 	$scope.selectedCat = $scope.categories[0];
 	$scope.showFollowing = true;
 
+	$scope.invokeRanking = function(itemRankArr) {
+		console.log("invokeRanking");
+		$(".input-rank").rating({
+			min: 0,
+			max: 5,
+			step: 0.01,
+			size: 'xs',
+			readonly: true,
+			showClear: false,
+			showCaption: false,
+			starCaptions: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',}
+		});
+		angular.forEach(itemRankArr, function(rank){
+			$('#rank'+rank.item_id).rating('update', rank.avg);
+			console.log(rank)
+		});
+	}
 
 	$scope.changeCategory = function() {
 		if ($scope.showFollowing) {
@@ -549,3 +600,134 @@ myApp.controller('addItem', function($scope,$resource, $location, $routeParams){
 		$location.path('/login').replace();
 	});
 });
+
+myApp.directive('myRepeatDirective', function() {
+	return function(scope, element, attrs) {
+		if (scope.$last){
+			console.log("invokeRanking");
+			console.log(element);
+			console.log($(".input-rank"));
+			$(".input-rank").rating({
+				min: 0,
+				max: 5,
+				step: 0.01,
+				size: 'xs',
+				readonly: true,
+				showClear: false,
+				showCaption: false,
+				starCaptions: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5'}
+			});
+
+			angular.forEach(scope.itemsArr, function (rank) {
+				console.log('#rank' + rank.item_id + " " + rank.avg);
+				console.log(document.querySelector("#rank" + rank.item_id));
+				angular.element(jQuery('#rank' + rank.item_id)).rating('update', rank.avg);
+			});
+
+		}
+	}
+});
+
+// I invoke the given expression when associated ngRepeat loop
+		// has finished its first round of rendering.
+		myApp.directive(
+			"repeatComplete",
+			function( $rootScope ) {
+
+				// Because we can have multiple ng-repeat directives in
+				// the same container, we need a way to differentiate
+				// the different sets of elements. We'll add a unique ID
+				// to each set.
+				var uuid = 0;
+
+
+				// I compile the DOM node before it is linked by the
+				// ng-repeat directive.
+				function compile( tElement, tAttributes ) {
+
+					// Get the unique ID that we'll be using for this
+					// particular instance of the directive.
+					var id = ++uuid;
+
+					// Add the unique ID so we know how to query for
+					// DOM elements during the digests.
+					tElement.attr( "repeat-complete-id", id );
+
+					// Since this directive doesn't have a linking phase,
+					// remove it from the DOM node.
+					tElement.removeAttr( "repeat-complete" );
+
+					// Keep track of the expression we're going to
+					// invoke once the ng-repeat has finished
+					// rendering.
+					var completeExpression = tAttributes.repeatComplete;
+
+					// Get the element that contains the list. We'll
+					// use this element as the launch point for our
+					// DOM search query.
+					var parent = tElement.parent();
+
+					// Get the scope associated with the parent - we
+					// want to get as close to the ngRepeat so that our
+					// watcher will automatically unbind as soon as the
+					// parent scope is destroyed.
+					var parentScope = ( parent.scope() || $rootScope );
+
+					// Since we are outside of the ng-repeat directive,
+					// we'll have to check the state of the DOM during
+					// each $digest phase; BUT, we only need to do this
+					// once, so save a referene to the un-watcher.
+					var unbindWatcher = parentScope.$watch(
+						function() {
+
+							console.info( "Digest running." );
+
+							// Now that we're in a digest, check to see
+							// if there are any ngRepeat items being
+							// rendered. Since we want to know when the
+							// list has completed, we only need the last
+							// one we can find.
+							var lastItem = parent.children( "*[ repeat-complete-id = '" + id + "' ]:last" );
+
+							// If no items have been rendered yet, stop.
+							if ( ! lastItem.length ) {
+
+								return;
+
+							}
+
+							// Get the local ng-repeat scope for the item.
+							var itemScope = lastItem.scope();
+
+							// If the item is the "last" item as defined
+							// by the ng-repeat directive, then we know
+							// that the ng-repeat directive has finished
+							// rendering its list (for the first time).
+							if ( itemScope.$last ) {
+
+								// Stop watching for changes - we only
+								// care about the first complete rendering.
+								unbindWatcher();
+
+								// Invoke the callback.
+								itemScope.$eval( completeExpression );
+
+							}
+
+						}
+					);
+
+				}
+
+				// Return the directive configuration. It's important
+				// that this compiles before the ngRepeat directive
+				// compiles the DOM node.
+				return({
+					compile: compile,
+					priority: 1001,
+					restrict: "A"
+				});
+
+			}
+		);
+
