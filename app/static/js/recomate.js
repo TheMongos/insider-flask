b98bd23e5e74cb56742cd65f72a4de7d027ddf98
eladbirnboim <eladbirn@gmail.com>
2015-08-15 23:22:05 +0300

    star ranking

--- app/static/js/recomate.js
+++ app/static/js/recomate.js
@@ -153,24 +153,6 @@ myApp.controller('user', function($scope, $resource, $location, $routeParams, Up
 		$scope.ranksArr =  Ranks.query();
 	}
 
-	$scope.invokeRanking = function() {
-		console.log("invokeRanking");
-		$(".input-rank").rating({
-			min: 0,
-			max: 5,
-			step: 0.01,
-			size: 'xs',
-			readonly: true,
-			showClear: false,
-			showCaption: false,
-			starCaptions: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',}
-		});
-		angular.forEach($scope.ranksArr, function(rank){
-			$('#rank'+rank.item_id).rating('update', rank.rank);
-			console.log(rank)
-		});
-	}
-
 	$scope.getReview = function(index, item_id){
 		var str2 = "review"+item_id;
 		var reviewBtn = "#reviewBtn"+item_id;
@@ -417,24 +399,6 @@ myApp.controller('top', function($scope,$resource, $location, $routeParams){
 	$scope.selectedCat = $scope.categories[0];
 	$scope.showFollowing = true;
 
-	$scope.invokeRanking = function() {
-		console.log("invokeRanking");
-		$(".input-rank").rating({
-			min: 0,
-			max: 5,
-			step: 0.01,
-			size: 'xs',
-			readonly: true,
-			showClear: false,
-			showCaption: false,
-			starCaptions: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',}
-		});
-		angular.forEach($scope.itemsArr, function(rank){
-			$('#rank'+rank.item_id).rating('update', rank.avg);
-			console.log(rank);
-		});
-	}
-
 	$scope.changeCategory = function() {
 		if ($scope.showFollowing) {
 			$scope.getTopFollowing();
@@ -572,6 +536,24 @@ myApp.controller('followers', function($scope,$resource, $location, $routeParams
 
 });
 
+myApp.directive('starRank', function() {
+  return {
+    link: function(scope, element, attrs) {
+		element.rating({
+			min: 0,
+			max: 5,
+			step: 0.01,
+			size: 'xs',
+			readonly: true,
+			showClear: false,
+			showCaption: false,
+			starCaptions: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',}
+		});
+		element.rating('update', attrs.starRank);
+    }
+  }
+});
+
 myApp.directive('errSrc', function() {
   return {
     link: function(scope, element, attrs) {
@@ -595,138 +577,3 @@ myApp.controller('addItem', function($scope,$resource, $location, $routeParams){
 		$location.path('/login').replace();
 	});
 });
-
-myApp.directive('myRepeatDirective', function() {
-	return function(scope, element, attrs) {
-		if (scope.$last){
-			console.log("invokeRanking");
-			console.log(element);
-			console.log($(".input-rank"));
-			$(".input-rank").rating({
-				min: 0,
-				max: 5,
-				step: 0.01,
-				size: 'xs',
-				readonly: true,
-				showClear: false,
-				showCaption: false,
-				starCaptions: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5'}
-			});
-			
-			angular.forEach(scope.itemsArr, function (rank) {
-				console.log('#rank' + rank.item_id + " " + rank.avg);
-				console.log('before' );
-				console.log(angular.element(jQuery('#rank' + rank.item_id)));
-				console.log('after');
-				jQuery('#rank' + rank.item_id).rating('update', rank.avg);
-				console.log(document.querySelector("#rank" + rank.item_id));
-				angular.element(jQuery('#rank' + rank.item_id)).rating('update', rank.avg);
-			});
-
-		}
-	}
-});
-
-// I invoke the given expression when associated ngRepeat loop
-		// has finished its first round of rendering.
-		myApp.directive(
-			"repeatComplete",
-			function( $rootScope ) {
-
-				// Because we can have multiple ng-repeat directives in
-				// the same container, we need a way to differentiate
-				// the different sets of elements. We'll add a unique ID
-				// to each set.
-				var uuid = 0;
-
-
-				// I compile the DOM node before it is linked by the
-				// ng-repeat directive.
-				function compile( tElement, tAttributes ) {
-
-					// Get the unique ID that we'll be using for this
-					// particular instance of the directive.
-					var id = ++uuid;
-
-					// Add the unique ID so we know how to query for
-					// DOM elements during the digests.
-					tElement.attr( "repeat-complete-id", id );
-
-					// Since this directive doesn't have a linking phase,
-					// remove it from the DOM node.
-					tElement.removeAttr( "repeat-complete" );
-
-					// Keep track of the expression we're going to
-					// invoke once the ng-repeat has finished
-					// rendering.
-					var completeExpression = tAttributes.repeatComplete;
-
-					// Get the element that contains the list. We'll
-					// use this element as the launch point for our
-					// DOM search query.
-					var parent = tElement.parent();
-
-					// Get the scope associated with the parent - we
-					// want to get as close to the ngRepeat so that our
-					// watcher will automatically unbind as soon as the
-					// parent scope is destroyed.
-					var parentScope = ( parent.scope() || $rootScope );
-
-					// Since we are outside of the ng-repeat directive,
-					// we'll have to check the state of the DOM during
-					// each $digest phase; BUT, we only need to do this
-					// once, so save a referene to the un-watcher.
-					var unbindWatcher = parentScope.$watch(
-						function() {
-
-							console.info( "Digest running." );
-
-							// Now that we're in a digest, check to see
-							// if there are any ngRepeat items being
-							// rendered. Since we want to know when the
-							// list has completed, we only need the last
-							// one we can find.
-							var lastItem = parent.children( "*[ repeat-complete-id = '" + id + "' ]:last" );
-
-							// If no items have been rendered yet, stop.
-							if ( ! lastItem.length ) {
-
-								return;
-
-							}
-
-							// Get the local ng-repeat scope for the item.
-							var itemScope = lastItem.scope();
-
-							// If the item is the "last" item as defined
-							// by the ng-repeat directive, then we know
-							// that the ng-repeat directive has finished
-							// rendering its list (for the first time).
-							if ( itemScope.$last ) {
-
-								// Stop watching for changes - we only
-								// care about the first complete rendering.
-								unbindWatcher();
-
-								// Invoke the callback.
-								itemScope.$eval( completeExpression );
-
-							}
-
-						}
-					);
-
-				}
-
-				// Return the directive configuration. It's important
-				// that this compiles before the ngRepeat directive
-				// compiles the DOM node.
-				return({
-					compile: compile,
-					priority: 1001,
-					restrict: "A"
-				});
-
-			}
-		);
-
