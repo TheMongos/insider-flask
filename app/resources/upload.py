7b4354f9911115700bef64f06aeac1e8d2f405c0
eladbirnboim <eladbirn@gmail.com>
2015-08-21 12:19:49 +0300

    remove image resize

--- app/resources/upload.py
+++ app/resources/upload.py
@@ -5,8 +5,6 @@ from flask_login import login_required
 from flask import session, current_app
 from . import api
 from .. import ALLOWED_EXTENSIONS
-import PIL
-from PIL import Image
 
 class Upload(Resource):
     decorators = [login_required]
@@ -21,7 +19,6 @@ class Upload(Resource):
             filename = secure_filename(user_image.filename)
 	    path = os.path.join(app.config['UPLOAD_FOLDER'], my_username + '.jpg')
             user_image.save(path)
-	    #resize_image(path)
             return_code = 200
             message = { 'status': 'success', 'message': 'image uploaded successfully.'}
 
@@ -37,16 +34,3 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-# resize the image to save loading times
-def resize_image(image_path):
-    basewidth = 500 
-    img = Image.open(image_path)
-    wpercent = (basewidth/float(img.size[0])) # calculate width %
-    hsize = int((float(img.size[1])*float(wpercent))) # calculate height
-    exif_flags = img._getexif()
-    if exif_flags and exif_flags[274] == 6: # check if image is portrait
-        rotate = True
-    img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
-    if rotate:
-        img = img.rotate(-90) 
-    img.save(image_path) 
